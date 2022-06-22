5307c610d19b599d5ebd9ed4b8509476282eaa5e
Sepanta <Sepanta_97@Yahoo.com>
2022-06-23 01:20:22 +0430

    connecting graph cms with our app

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,37 @@
 import Head from "next/head";
-import { PostCard, PostWidget, Categories, } from "../components/index";
+import { PostCard, PostWidget, Categories } from "../components/index";
+import { getPosts } from "../services/index";
+// const posts = [
+//   {
+//     title: "Every Superhero shouldnt exsit!",
+//     excerpt: "I Hate Aquaman",
+//     id: 1,
+//   },
+//   {
+//     title: "Lets know more about Batman!",
+//     excerpt: "This guy doesn't have any superpower !!!",
+//     id: 2,
+//   },
+// ];
 
-const posts = [
-  {
-    title: "Every Superhero shouldnt exsit!",
-    excerpt: "I Hate Aquaman",
-    id: 1,
-  },
-  {
-    title: "Lets know more about Batman!",
-    excerpt: "This guy doesn't have any superpower !!!",
-    id: 2,
-  },
-];
-
-export default function Home() {
+export default function Home({ posts }) {
+  if (!posts) return <p>loading ...</p>;
+  console.log(posts);
   return (
     <>
       <Head>
         <title>DC BLOG</title>
         <link rel="icon" href="/batman.png"></link>
       </Head>
-     
-      <div className="container  px-10 mb-8 ">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-          <div className="lg:col-span-8 col-span-1">
+
+      <div className="container px-10 mb-8 ">
+        <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+          <div className="col-span-1 lg:col-span-8">
             {posts.map(({ title, excerpt, id }, i) => (
               <PostCard key={id} title={title} excerpt={excerpt} />
             ))}
           </div>
-          <div className="lg:col-span-4 col-span-1">
+          <div className="col-span-1 lg:col-span-4">
             <div className="relative top-8 lg:sticky">
               <PostWidget />
               <Categories />
@@ -40,3 +42,13 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps = async (ctx) => {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
