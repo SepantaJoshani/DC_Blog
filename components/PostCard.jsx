07c687b30ea54ec394abfd62bbc46fa68c5f2819
Sepanta <Sepanta_97@Yahoo.com>
2022-06-23 01:57:06 +0430

    first card

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import moment from "moment";
+import Link from "next/link";
+import Image from "next/image";
 
-const PostCard = ({ title, excerpt }) => {
+const PostCard = ({ post }) => {
   return (
-    <div className="text-white">
-      {title}
-      {excerpt}
-    </div>
+    <article className="p-0 pb-12 mb-8 bg-white rounded-lg shadow-lg lg:p-8">
+      <div className="relative mb-6 overflow-hidden shadow-md">
+        <Image
+          src={post.featuredImage.url}
+          alt={post.title}
+          layout="responsive"
+          width={280}
+          height={240}
+        />
+      </div>
+      <h1 className="mb-8 text-3xl font-semibold text-center transition duration-700 cursor-pointer hover:text-pink-600">
+        <Link href={`/post/${post.slug}`}>{post.title}</Link>
+      </h1>
+    </article>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,6 @@
 import Head from "next/head";
 import { PostCard, PostWidget, Categories } from "../components/index";
 import { getPosts } from "../services/index";
-// const posts = [
-//   {
-//     title: "Every Superhero shouldnt exsit!",
-//     excerpt: "I Hate Aquaman",
-//     id: 1,
-//   },
-//   {
-//     title: "Lets know more about Batman!",
-//     excerpt: "This guy doesn't have any superpower !!!",
-//     id: 2,
-//   },
-// ];
 
 export default function Home({ posts }) {
   if (!posts) return <p>loading ...</p>;
@@ -27,8 +15,8 @@ export default function Home({ posts }) {
       <div className="container px-10 mb-8 ">
         <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
           <div className="col-span-1 lg:col-span-8">
-            {posts.map(({ title, excerpt, id }, i) => (
-              <PostCard key={id} title={title} excerpt={excerpt} />
+            {posts.map((post, i) => (
+              <PostCard key={post.node.title} post={post.node} />
             ))}
           </div>
           <div className="col-span-1 lg:col-span-4">
